ce6fc15370171235076a8aaa4c899a861e4d7819
nachomallavia <nachomallavia@hotmail.com>
2024-12-23 10:25:42 -0300

    Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import svelte from "@astrojs/svelte";
-import node from "@astrojs/node";
+import svelte from '@astrojs/svelte';
+import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: "server",
-  integrations: [tailwind(), svelte()],
-  adapter: node({
-    mode: "standalone"
-  })
+    output: 'server',
+    integrations: [tailwind(), svelte()],
+    adapter: netlify(),
 });
